a0809a5d829366a1ff95f0a97c09c1fc9b567303
Prajwal Udapudi <prajwaludapudi0@gmail.com>
2024-08-30 23:16:40 +0530

    first commit

---
+++ src/components/flexbet.js
@@ -0,0 +1,8 @@
+import { Box,styled} from "@mui/material";
+
+const FlexBetween=styled(Box)({
+    display:"flex",
+    justifyContent:"center",
+    alignItems:"center"
+})
+export default FlexBetween;

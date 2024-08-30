d222a9ffc8a2b2a5ea22669344674600a4c346b3
Prajwal Udapudi <92397189+prajwaluda@users.noreply.github.com>
2024-08-30 23:07:45 +0530

    Delete client directory

--- client/src/components/flexbet.js
+++
@@ -1,8 +0,0 @@
-import { Box,styled} from "@mui/material";
-
-const FlexBetween=styled(Box)({
-    display:"flex",
-    justifyContent:"center",
-    alignItems:"center"
-})
-export default FlexBetween;

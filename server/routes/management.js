f98f8dc677fa89c4222c8f29f979ce46c5aa0c5d
Prajwal Udapudi <92397189+prajwaluda@users.noreply.github.com>
2024-08-30 23:05:03 +0530

    Delete server directory
    jvn

--- server/routes/management.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import {getAdmins} from "../controllers/management.js"
-import {getUserPerformance} from "../controllers/management.js"
-
-const router =express.Router();
-router.get("/admins",getAdmins)
-router.get("/performance/:id",getUserPerformance)
-
-export default router;
--- server/routes/sales.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getOverallStat } from "../controllers/sales.js";
-
-const router =express.Router();
-router.get("/overview",getOverallStat);
-
-export default router;

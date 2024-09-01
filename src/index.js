bc240904c62d1a602badf58e191712ebecb7b7ee
Prajwal Udapudi <prajwaludapudi0@gmail.com>
2024-09-01 17:14:31 +0530

    cicd6

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const store = configureStore({
 });
 setupListeners(store.dispatch);
 
-
+console.log(process.env.REACT_APP_BASE_URL)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>

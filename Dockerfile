67de549a1c2d3346e1ca050872efd255b6d7e1fc
Prajwal Udapudi <prajwaludapudi0@gmail.com>
2024-09-01 19:04:14 +0530

    cicd7

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,5 @@
 FROM node:alpine3.18 as build
 
-ARG REACT_APP_BASE_URL
-
-ENV REACT_APP_BASE_URL=$REACT_APP_BASE_URL
 
 WORKDIR /app
 COPY package.json .
--- src/state/api.js
+++ src/state/api.js
@@ -1,6 +1,5 @@
 import {createApi,fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
-console.log(process.env.REACT_APP_BASE_URL)
 export const api =createApi({
     baseQuery:fetchBaseQuery({baseUrl:process.env.REACT_APP_BASE_URL}),
     reducerPath:"adminApi",
